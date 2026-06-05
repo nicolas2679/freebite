335338b1707f5f38148d0521f13a03cbde46e92a
nmillig2 <nmillig2@uwo.ca>
2026-06-05 01:26:28 -0400

    Changed site to vite react

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="site">
+      <h1>FreeBite</h1>
+      <p>FreeBite coming soon! By Emily Chan and Nicolas Milligan-Torres</p>
+    </main>
+  );
+}
